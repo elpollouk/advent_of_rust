3f9963993ecf0aa44e9f6e84ae94ef74fe6644b7
Adrian O'Grady <elpollouk@users.noreply.github.com>
2018-12-05 22:16:40 +0000

    Day 1 testing

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,31 @@
+use std::collections::HashSet;
+
+fn problem1 (frequencies: &[i32]) -> i32 {
+    let mut current_frequency = 0;
+
+    for f in frequencies.iter() {
+        current_frequency += f;
+    }
+
+    current_frequency
+}
+
+fn problem2 (frequencies: &[i32]) -> i32 {
+    let mut current_frequency = 0;
+    let mut seen: HashSet<i32> = HashSet::new();
+    let mut i = 0;
+
+    while !seen.contains(&current_frequency) {
+        seen.insert(current_frequency);
+        current_frequency += frequencies[i % frequencies.len()];
+        i += 1;
+    }
+
+    current_frequency
+}
+
 pub fn solve() {
     println!("Day 01");
+    println!("  Test 1 = {}", problem1(&[1, -2, 3, 1]));
+    println!("  Test 2 = {}", problem2(&[7, 7, -2, -7, -4]));
 }
